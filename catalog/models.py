ba459e5ea98053fd2b33e930219910e80a91b1b0
Tokomichel <tokowouendeu@gmail.com>
2026-08-20 22:59:39 +0200

    clotiring catalog app for now

--- catalog/models.py
+++ catalog/models.py
@@ -6,15 +6,24 @@ class Artist(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Album(models.Model):
     title = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     release_date = models.DateField()
     cover_url = models.URLField()
 
+    def __str__(self):
+        return self.title
+
 class Track(models.Model):
     title = models.CharField(max_length=200)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     duration = models.FloatField()
-    file_url = models.URLField()
+    file_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
